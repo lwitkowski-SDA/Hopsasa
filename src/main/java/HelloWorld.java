8504fe2654e4007da51f5bcf75f39c3d7920206e
lwitkowski-SDA <lwitkowskigw@gmail.com>
2021-04-19 20:36:03 +0200

    dodałem swój wpis do wydrukowania na konsolę

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        System.out.println("tutaj Lukasz Witkowski, 20:17");
+        System.out.println("tutaj Lukasz Witkowski, 20:35");
     }
 }
