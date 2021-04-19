e4a9f11234a026d1be9e445757498d24f2e72492
Salomonik <salomonik@gmail.com>
2021-04-19 19:44:16 +0100

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello to ja Jakub, jest godz 19:40");
+
     }
+
 }
