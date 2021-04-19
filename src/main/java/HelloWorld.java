bf78c4ac29c9e5859f38d5f4752c9b9375db2994
Cirind <cezary150199@gmail.com>
2021-04-19 20:22:56 +0200

    dodałem swój wpis

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        System.out.println("tutaj Cezary Janowski, 20:21")
+        System.out.println("tutaj Cezary Janowski, 20:23");
     }
 }
